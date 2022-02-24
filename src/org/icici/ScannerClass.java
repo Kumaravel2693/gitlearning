cefaf5be2eef483e9fdb53660bab6a2cadb2e2bf
Kumaravel2693 <kumaravelramesh93@gmail.com>
2022-02-24 11:53:31 +0530

    pushafteredit

--- src/org/icici/ScannerClass.java
+++ src/org/icici/ScannerClass.java
@@ -7,15 +7,18 @@ public class ScannerClass {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		
-		System.out.println("Enter Name, Age, Salary");
+		System.out.println("Enter Name, Age, Salary, ID");
 		
 		String name = sc.nextLine();
 		int Age = sc.nextInt();
 		double salary = sc.nextDouble();
+		int id = sc.nextInt();
+		
 		
 		System.out.println("Name is:" +name);
 		System.out.println("Age is:" +Age);
 		System.out.println("Salaey is:" +salary);
+	    System.out.println("ID Is: " +id);
 	}
 
 }
